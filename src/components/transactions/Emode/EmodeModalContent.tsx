4142a866ca34f9bd6dd1dc2fd572449160b87ec7
Sam Mason de Caires <sam.m@aave.com>
2026-03-18 14:07:47 +0000

    feat: replace emode label underscore with spaces

--- src/components/transactions/Emode/EmodeModalContent.tsx
+++ src/components/transactions/Emode/EmodeModalContent.tsx
@@ -28,6 +28,7 @@ import { useModalContext } from 'src/hooks/useModal';
 import { useWeb3Context } from 'src/libs/hooks/useWeb3Context';
 import { useRootStore } from 'src/store/root';
 import { getNetworkConfig } from 'src/utils/marketsAndNetworksConfig';
+import { replaceUnderscoresWithSpaces } from 'src/utils/utils';
 import { useShallow } from 'zustand/shallow';
 
 import { TxErrorView } from '../FlowCommons/Error';
@@ -360,7 +361,7 @@ export const EmodeModalContent = ({ user }: { user: ExtendedFormattedUser }) => 
                           sx={{ opacity: emode.available ? 1 : 0.5 }}
                           fontStyle={emode.available ? 'normal' : 'italic'}
                         >
-                          {emode.label}
+                          {replaceUnderscoresWithSpaces(emode.label)}
                         </Typography>
                         {emode.id === user.userEmodeCategoryId && (
                           <Box sx={{ display: 'inline-flex', alignItems: 'center' }}>
